4caa3afcc01f5ed5e485041d4c9e905164f31f6f
Alexey Velikiy <gmpota@gmail.com>
2017-03-13 11:11:19 +0100

    some comit

--- src/pics/von-neuman-1.vue
+++ src/pics/von-neuman-1.vue
@@ -15,7 +15,7 @@ TODO:
       <block width="190" height="185" x="0" y="0">
         <text x="95" y="20" text-anchor="middle"> CPU </text>
         <block width="180" height="70" x="5" y="30" label="Control Unit" />
-        <block width="180" height="70" x="5" y="110" label="ALU" />
+        <block width="180" height="70" x="5" y="105" label="ALU" />
       </block>
       <block width="180" height="70" x="300" y="40" label="Memory" />
       <dearrow x1="190" y1="75" x2="300" y2="75" />
