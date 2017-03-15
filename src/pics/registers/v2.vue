3b854ccc96d09b618712e7e0696654c1da537bf1
Alexey Velikiy <gmpota@gmail.com>
2017-03-15 09:08:33 +0100

    rv2 reg vals

--- src/pics/registers/v2.vue
+++ src/pics/registers/v2.vue
@@ -5,17 +5,17 @@
     </g>
     <block width="180" :height="240" x="5" y="181">
       <blockTitle>Registers</blockTitle>
-      <rb :x="leftX" :y="line(1)" value="FFAA0013">rax</rb>
-      <rb :x="leftX" :y="line(2)" value="FFAA0013">rbx</rb>
-      <rb :x="leftX" :y="line(3)" value="FFAA0013">rcx</rb>
-      <rb :x="leftX" :y="line(4)" value="FFAA0013">rdx</rb>
-      <rb :x="leftX" :y="line(5.2)" value="FFAA0013">rip</rb>
+      <rb :x="leftX" :y="line(1)"   value="d64905e5">rax</rb>
+      <rb :x="leftX" :y="line(2)"   value="68d10711">rbx</rb>
+      <rb :x="leftX" :y="line(3)"   value="686db5c5">rcx</rb>
+      <rb :x="leftX" :y="line(4)"   value="735437d4">rdx</rb>
+      <rb :x="leftX" :y="line(5.2)" value="72396d33">rip</rb>
 
-      <rb :x="rightX" :y="line(1)" value="FFAA0013">rsi</rb>
-      <rb :x="rightX" :y="line(2)" value="FFAA0013">rdi</rb>
-      <rb :x="rightX" :y="line(3)" value="FFAA0013">rsp</rb>
-      <rb :x="rightX" :y="line(4)" value="FFAA0013">rbp</rb>
-      <rb :x="rightX" :y="line(5.2)" value="FFAA0013">rflags</rb>
+      <rb :x="rightX" :y="line(1)" value="e2809473">rsi</rb>
+      <rb :x="rightX" :y="line(2)" value="534c972c">rdi</rb>
+      <rb :x="rightX" :y="line(3)" value="b3ad80ab">rsp</rb>
+      <rb :x="rightX" :y="line(4)" value="75732e3e">rbp</rb>
+      <rb :x="rightX" :y="line(5.2)" value="ff8b2489">rflags</rb>
     </block>
   </svg>
 </template>
@@ -28,10 +28,10 @@ export default {
   },
   computed: {
     leftX: function functionName() {
-      return 45;
+      return 46.5;
     },
     rightX: function functionName() {
-      return 135;
+      return 133.5;
     },
   },
   methods: {
