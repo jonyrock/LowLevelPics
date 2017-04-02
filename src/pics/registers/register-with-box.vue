f66925325a026e618c16c5f1935ebf25ec98af68
Alexey Velikiy <gmpota@gmail.com>
2017-04-03 00:45:46 +0300

    fat regs

--- src/pics/registers/register-with-box.vue
+++ src/pics/registers/register-with-box.vue
@@ -2,12 +2,14 @@
   <g>
     <r :x="x" :y="y"> {{ name }} </r>
     <g :transform="transform">
-      <boxedText :value='value'/>
+      <boxedText :value='value1'/>
+      <boxedText :value='value2' y="29"/>
     </g>
   </g>
 </template>
 
 <script>
+const _ = require('lodash');
 export default {
   props: ['x', 'y', 'value'],
   components: {
@@ -19,7 +21,18 @@ export default {
     },
     transform: function() {
       return `translate(${this.x - 40} ${this.y + 5}) scale(0.5)`
-    }
+    },
+    value1: function() {
+      return _.take(this.value, 8).join('')
+    },
+    value2: function() {
+      return _.drop(this.value, 8).join('')
+    },
+    // dvalue: function() {
+    //   return _.take(this.value, 2).join('') +
+    //           '...' +
+    //           _.drop(this.value, 5).join('');
+    // }
   },
   data: function() {
     return {}
--- src/pics/registers/v2.vue
+++ src/pics/registers/v2.vue
@@ -1,21 +1,21 @@
 <template>
-  <svg width="192" height="432">
+  <svg width="192" height="482">
     <g transform="translate(1,1)" class="nonactive">
-      <cpuBase :height="430" />
+      <cpuBase :height="480" />
     </g>
-    <block width="180" :height="240" x="5" y="181">
+    <block width="180" :height="287" x="5" y="181">
       <blockTitle>Registers</blockTitle>
-      <rb :x="leftX" :y="line(1)"   value="D64905E5">rax</rb>
-      <rb :x="leftX" :y="line(2)"   value="68D10711">rbx</rb>
-      <rb :x="leftX" :y="line(3)"   value="686DB5C5">rcx</rb>
-      <rb :x="leftX" :y="line(4)"   value="735437D4">rdx</rb>
-      <rb :x="leftX" :y="line(5.2)" value="72396D33">rip</rb>
+      <rb :x="leftX" :y="line(1)"   value="D64905E5E92004D3">rax</rb>
+      <rb :x="leftX" :y="line(2)"   value="68D1071186DB5372">rbx</rb>
+      <rb :x="leftX" :y="line(3)"   value="686DB5C539637133">rcx</rb>
+      <rb :x="leftX" :y="line(4)"   value="735437D42396E5A1">rdx</rb>
+      <rb :x="leftX" :y="line(5.2)" value="72396437DD396E33">rip</rb>
 
-      <rb :x="rightX" :y="line(1)" value="E2809473">rsi</rb>
-      <rb :x="rightX" :y="line(2)" value="534C972C">rdi</rb>
-      <rb :x="rightX" :y="line(3)" value="B3AD80AB">rsp</rb>
-      <rb :x="rightX" :y="line(4)" value="75732E3E">rbp</rb>
-      <rb :x="rightX" :y="line(5.2)" value="FF8B2489">rflags</rb>
+      <rb :x="rightX" :y="line(1)"   value="E280947337D42391">rsi</rb>
+      <rb :x="rightX" :y="line(2)"   value="534C972C3AD1248F">rdi</rb>
+      <rb :x="rightX" :y="line(3)"   value="B3AD80ABD802E112">rsp</rb>
+      <rb :x="rightX" :y="line(4)"   value="75732E3E8B7D4211">rbp</rb>
+      <rb :x="rightX" :y="line(5.2)" value="FF8B2489118BD2B5">rflags</rb>
     </block>
   </svg>
 </template>
@@ -36,7 +36,7 @@ export default {
   },
   methods: {
     line: function(l) {
-      return l * 40 + 5;
+      return l * 49 - 10;
     }
   }
 }
